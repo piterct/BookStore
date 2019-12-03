8fff76ca1f4316bdeac531e683e6a15a041b4f02
Michael Peter <michael_piterct@hotmail.com>
2019-12-02 23:51:36 -0300

    #8 Trafegando dados para a View

---
+++ BookStore/BookStore/Controllers/TesteController.cs
@@ -0,0 +1,43 @@
+﻿using BookStore.Domain;
+using System.Web.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class TesteController : Controller
+    {
+        public ViewResult Dados(int Id)
+        {
+            var autor = new Autor
+            {
+                Id = 1,
+                Nome = "Michael Peter"
+            };
+
+            ViewBag.Categoria = "Produtos de Limpeza";
+            ViewData["Categoria"] = "Produtos de Informática";
+            TempData["Categoria"] = "Produtos de Escritório";
+            Session["Categoria"] = "Móveis";
+
+            return View(autor);
+        }
+
+        public JsonResult UmaAction(int id, string nome)
+        {
+
+            var autor = new Autor
+            {
+                Id = id,
+                Nome = nome
+            };
+
+            return Json(autor, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        [ActionName("Autor")]
+        public JsonResult ActionDois(Autor autor)
+        {
+            return Json(autor);
+        }
+    }
+}
