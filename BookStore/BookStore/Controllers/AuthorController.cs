79b34e0c4f9a69f7ac77e6924711b811001f4f9c
Michael Peter <michael_piterct@hotmail.com>
2020-06-26 09:56:13 -0300

    # Action Filters

--- BookStore/BookStore/Controllers/AuthorController.cs
+++ BookStore/BookStore/Controllers/AuthorController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BookStore.Filters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,7 @@ using System.Web.Routing;
 namespace BookStore.Controllers
 {
     [RoutePrefix("autores")]
+    [LogActionFilter()]
     public class AuthorController : Controller
     {
         [Route("listar")]
