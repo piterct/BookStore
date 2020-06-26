67912c6f592091ad08d4ea3b828c521978c0d9ed
Michael Peter <michael_piterct@hotmail.com>
2020-06-26 13:22:24 -0300

    repository pattern

--- BookStore/BookStore/Controllers/AuthorController.cs
+++ BookStore/BookStore/Controllers/AuthorController.cs
@@ -1,4 +1,7 @@
-﻿using BookStore.Filters;
+﻿using BookStore.Domain;
+using BookStore.Filters;
+using BookStore.Repositories;
+using BookStore.Repositories.Contracts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +15,18 @@ namespace BookStore.Controllers
     //[LogActionFilter()]
     public class AuthorController : Controller
     {
+        private IAuthorRepository repository;
+
+        public AuthorController()
+        {
+            repository = new AuthorRepository();
+        }
+
         [Route("listar")]
         public ActionResult Index()
         {
-            return View();
+            var autores = repository.Get();
+            return View(autores);
         }
 
         [Route("criar")]
@@ -24,16 +35,51 @@ namespace BookStore.Controllers
             return View();
         }
 
+        [Route("criar")]
+        [HttpPost]
+        public ActionResult Create(Autor author)
+        {
+            if (repository.Create(author))
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(author);
+        }
+
         [Route("editar/{id:int}")]
         public ActionResult Edit(int id)
         {
-            return View();
+            var author = repository.Get(id);
+            return View(author);
+        }
+
+        [Route("editar/{id:int}")]
+        [HttpPost]
+        public ActionResult Edit(Autor author)
+        {
+            if (repository.Update(author))
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(author);
         }
 
         [Route("excluir/{id:int}")]
         public ActionResult Delete(int id)
         {
-            return View();
+            var author = repository.Get(id);
+            return View(author);
+        }
+
+        [Route("excluir/{id:int}")]
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirm(int id)
+        {
+            repository.Delete(id);
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ BookStore/BookStore/Repositories/Contracts/IAuthorRepository.cs
@@ -0,0 +1,18 @@
+﻿using BookStore.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Repositories.Contracts
+{
+    public interface IAuthorRepository : IDisposable
+    {
+        List<Autor> Get();
+        Autor Get(int id);
+        List<Autor> GetByName(string name);
+        bool Create(Autor autor);
+        bool Update(Autor autor);
+        void Delete(int autor);
+    }
+}
