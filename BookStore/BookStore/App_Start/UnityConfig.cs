9b280e593ca7d21828851e6c3684535508d39f27
Michael Peter <michael_piterct@hotmail.com>
2020-06-27 07:58:28 -0300

    injection dependency

---
+++ BookStore/BookStore/App_Start/UnityConfig.cs
@@ -0,0 +1,29 @@
+﻿using BookStore.Context;
+using BookStore.Repositories;
+using BookStore.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Unity;
+using Unity.AspNet.Mvc;
+
+namespace BookStore.App_Start
+{
+    public class UnityConfig
+    {
+        public static void RegisterComponents()
+        {
+            var container = new UnityContainer();
+
+
+            // register all your components with the container here
+            // it is NOT necessary to register your controllers
+            container.RegisterType<BookStoreDataContext, BookStoreDataContext>();
+            container.RegisterType<IAuthorRepository, AuthorRepository>();
+
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+        }
+    }
+}
