63e034b7f0c93b72806841e64fe75ecf9e4e88d6
Michael Peter <michael_piterct@hotmail.com>
2020-07-04 20:57:23 -0300

    # configure bundle

--- BookStore/BookStore/App_Start/BundleConfig.cs
+++ BookStore/BookStore/App_Start/BundleConfig.cs
@@ -25,6 +25,9 @@ namespace BookStore
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/custom").Include(
+                      "~/Content/styles.css"));
         }
     }
 }
